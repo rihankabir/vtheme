65fc7e2098447039414547b9357125f30b93226b
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-13 04:12:34 +0600

    added reusable get template part for hero in index file

--- index.php
+++ index.php
@@ -1,27 +1,9 @@
 <?php get_header() ; ?>
 
   <!-- ======= Header ======= -->
- 
-  <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">Grow your business with Vespa</h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">We are team of talanted designers making websites with Bootstrap</h2>
-          <div data-aos="fade-up" data-aos-delay="800">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
-          </div>
-        </div>
-        <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-img.png" class="img-fluid animated" alt="">
-        </div>
-      </div>
-    </div>
-
-  </section><!-- End Hero -->
-
+ <?php
+ get_template_part('template-parts/hero');
+ ?>
   <main id="main">
 
     <!-- ======= Clients Section ======= -->
