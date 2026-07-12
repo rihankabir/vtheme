3f43ac0130f080bf036e620b9edc89f9b1f37a40
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-13 02:11:22 +0600

    added dynamic language attr and dynamic body class

--- functions.php
+++ functions.php
@@ -5,9 +5,19 @@
 
 function vtheme_setup(){
 
+
+add_theme_support('custom-logo');
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
-add_theme_support('custom-logo');
+
+register_nav_menus(
+array(
+    'primary_menu' => 'Primary Menu'
+)
+
+);
+
+
 
 }
 
--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?> >
 
 <head>
   <meta charset="utf-8">
@@ -17,4 +17,4 @@
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?> >
