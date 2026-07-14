798c55916b89f7e578fea248411a04f840e5126a
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-15 01:55:27 +0600

    added customize setting for vtheme phone

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,14 +1,19 @@
 <?php 
 
 function vtheme_customize_register( $wp_customize ) {
-    
+
 $wp_customize -> add_section(
     'vtheme_contact_section',array(
         'title' => 'Contact_Information',
         'priority' => 30,
     )
 );
-
+$wp_customize -> add_setting(
+    'vtheme_phone',array(
+        'default' => '',
+        'sanitize_callback' => 'sanitize_text_field',
+    )
+);
 
 }
 
