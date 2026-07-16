e16a24010cecdf3a0713d0de660b6ab3bcdc059a
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-16 17:31:10 +0600

    added first custom post types register in post-types file

--- inc/post-types.php
+++ inc/post-types.php
@@ -2,14 +2,37 @@
 
 function vtheme_register_post_types(){
 
+register_post_type(
+'service',
+array(
+    'labels' => array(
+        'name' => ('services'),
+'singular_name' => ('service'),
+    ),
+    'public' => true,
+    'menu_position'=> 5,
+    'menu_icon'    => 'dashicons-admin-tools',
+            'supports'     => array(
+                'title',
+                'editor',
+                'thumbnail',
+            ),
+             'has_archive'  => true,
+            'rewrite'      => array(
+                'slug' => 'services',
+            ),
+            'show_in_rest' => true,
+)
 
+
+ );
 
 
 
 
 
 }
-
+add_action('init', 'vtheme_register_post_types');
 
 
 
