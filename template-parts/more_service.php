b558bf05296b6b701e0d1eb08f61e736e33e2e61
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-16 19:53:10 +0600

    added the wp quaery loop in the more services section and removed the hardcoded content

--- template-parts/more_service.php
+++ template-parts/more_service.php
@@ -2,43 +2,34 @@
       <div class="container">
 
         <div class="row">
-          <div class="col-md-6 d-flex align-items-stretch">
-            <div class="card" data-aos="fade-up" data-aos-delay="100">
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Lobira Duno</a></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.</p>
-                <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
-            <div class="card" data-aos="fade-up" data-aos-delay="100">
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Limere Radses</a></h5>
-                <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem.</p>
-                <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-              </div>
-            </div>
+        <?php
+        $item = array(
+'post_type' => 'more_service',
+'post_per_page' => 6,
+'order'=> 'ASC',
+        );
+        $more_services = new WP_Query($item);
+if($more_services-> have_posts()) : while($more_services-> have_posts()): 
+  
+  $more_services-> the_post();
 
-          </div>
-          <div class="col-md-6 d-flex align-items-stretch mt-4">
+        ?>
+<div class="col-md-6 d-flex align-items-stretch mt-4">
             <div class="card" data-aos="fade-up" data-aos-delay="100">
               <div class="card-body">
-                <h5 class="card-title"><a href="">Nive Lodo</a></h5>
-                <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores.</p>
-                <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
+                <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?> </a></h5>
+                <p class="card-text"><?php the_content(); ?> </p>
+                <div class="read-more"><a href="<?php the_permalink( ); ?>"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
           </div>
-          <div class="col-md-6 d-flex align-items-stretch mt-4">
-            <div class="card" data-aos="fade-up" data-aos-delay="100">
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Pale Treda</a></h5>
-                <p class="card-text">Nostrum eum sed et autem dolorum perspiciatis. Magni porro quisquam laudantium voluptatem.</p>
-                <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
-              </div>
-            </div>
-          </div>
+          <?php 
+          endwhile;
+          wp_reset_postdata(  );
+        endif;
+        ?>
+
+
         </div>
 
       </div>
