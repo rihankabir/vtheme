eff2ee55981c0530bcde3b51a8c9904eeae3f8c7
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-17 20:47:08 +0600

    modified the head title

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="<?php bloginfo('charset'); ?> ">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Vespa</title>
+  <title>Vtheme</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
