57b233809b4aa4ff436c7a14b68ea882fb7806df
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-17 19:43:42 +0600

    added core content in the index file as backup options

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<main class="container py-5">
+
+<?php
+if ( have_posts() ) :
+
+    while ( have_posts() ) :
+
+        the_post();
+
+        the_content();
+
+    endwhile;
+
+else :
+
+    echo '<p>No content found.</p>';
+
+endif;
+?>
+
+</main>
+
+<?php get_footer(); ?>
