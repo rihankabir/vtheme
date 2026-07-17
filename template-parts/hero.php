620745cba51d0739c4dc449ffede894834584f6e
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-17 20:33:38 +0600

    update the hero content with theme mod customizer content for dynamic

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -5,14 +5,14 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">Grow your business with Vespa</h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">We are team of talanted designers making websites with Bootstrap</h2>
+          <h1 data-aos="fade-up"><?php echo esc_html(get_theme_mod('hero_title')); ?></h1>
+          <h2 data-aos="fade-up" data-aos-delay="400"><?php echo esc_html(get_theme_mod('hero_subtitle')); ?></h2>
           <div data-aos="fade-up" data-aos-delay="800">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
+            <a href="<?php echo esc_url(get_theme_mod('hero_button_url')); ?>" class="btn-get-started scrollto"><?php echo esc_html(get_theme_mod('hero_button_text')); ?></a>
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-          <img src="<?php echo get_template_directory_uri(); ?>/assets/img/hero-img.png" class="img-fluid animated" alt="">
+          <img src="<?php echo esc_url(get_theme_mod('hero_image')); ?>"class="img-fluid animated" alt="">
         </div>
       </div>
     </div>
