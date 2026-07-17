01acbf2d8daeba1b9bcc294830344ed2b766472a
Md.Rihanul Kabir <rihankabir006@gmail.com>
2026-07-17 19:31:48 +0600

    added customization setting and control for hero section image

--- inc/customizer.php
+++ inc/customizer.php
@@ -185,6 +185,21 @@ $wp_customize->add_control(
         'type'    => 'url',
     )
 );
+$wp_customize->add_setting(
+    'hero_image'
+);
+
+$wp_customize->add_control(
+    new WP_Customize_Image_Control(
+        $wp_customize,
+        'hero_image',
+        array(
+            'label'   => 'Hero Image',
+            'section' => 'vtheme_hero_section',
+        )
+    )
+);
+
 
 }
 
